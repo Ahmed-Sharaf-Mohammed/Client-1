4d50316421c3209eb749273b0f58d5b23790d552
Ahmed Sharaf Mohammed Hassan <Ahmed.Sharaf.Mohammed.Hassan@gmail.com>
2023-07-12 17:06:00 +0300

    0

--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,5 @@ import subprocess, os, mtranslate,polib
 from third_party_app.views import make_messages, translate_po_file, compile_translations
 
 
+def home(request):
+    return render(request, 'accounts/index.html')
