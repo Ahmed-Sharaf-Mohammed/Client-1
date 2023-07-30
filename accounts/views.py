da4004b94052d96622a3b564f0df1cb803abc694
Ahmed Sharaf Mohammed Hassan <Ahmed.Sharaf.Mohammed.Hassan@gmail.com>
2023-07-30 20:19:08 +0300

    Revert "0"
    This reverts commit 4d50316421c3209eb749273b0f58d5b23790d552.

--- accounts/views.py
+++ accounts/views.py
@@ -3,5 +3,3 @@ import subprocess, os, mtranslate,polib
 from third_party_app.views import make_messages, translate_po_file, compile_translations
 
 
-def home(request):
-    return render(request, 'accounts/index.html')
